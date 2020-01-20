55e55fab5627e91faf40d37e8d5b16dabfdef404
Polovni <57703227+Polovni@users.noreply.github.com>
2020-01-20 09:50:28 +0100

    added all files

---
+++ Vertex.py
@@ -0,0 +1,12 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Vertex:
+  def __init__(self, value):
+    self.value = value
+    self.edges = {}
+
+  def add_edge(self, vertex, weight = 0):
+    self.edges[vertex] = weight
+
+  def get_edges(self):
+    return list(self.edges.keys())
